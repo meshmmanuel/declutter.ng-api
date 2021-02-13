7d7df2158b326a3995f341280413c1b7f4b3f307
Mesh Manuel <meshmmanuel@yahoo.com>
2021-02-13 21:12:00 +0100

    add customer info to model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
 {
     use HasFactory, SoftDeletes, Filterable;
 
-    protected $fillable = ['name', 'description', 'selling_price', 'user_id', 'release_date', 'status', 'product_status'];
+    protected $fillable = ['name', 'description', 'selling_price', 'user_id', 'release_date', 'status', 'product_status', 'customer_name', 'customer_phone'];
     protected $with = ['files', 'defect'];
 
     // Relationships
